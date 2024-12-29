c3937242860283ce16659dfe0c9fb41bd3f3af1b
repleyva <rusvelpassos@gmail.com>
2024-12-28 20:22:59 -0500

    feature: add Themes

--- app/src/main/java/com/repleyva/tempus/presentation/theme/Theme.kt
+++ app/src/main/java/com/repleyva/tempus/presentation/theme/Theme.kt
@@ -11,34 +11,35 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+    primary = brandBlue100Night,
+    onPrimary = brandBlue50Night,
+    secondary = textSecondaryNight,
+    tertiary = textPrimaryNight,
+    background = backgroundPrimaryNight,
+    error = systemErrorNight,
+    surface = bottomNavigationBackgroundNight,
+    onSurface = backgroundSecondaryNight,
+
+    )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = brandBlue100Light,
+    onPrimary = brandBlue50Light,
+    secondary = textSecondaryLight,
+    tertiary = textPrimaryLight,
+    background = backgroundPrimaryLight,
+    error = systemErrorLight,
+    surface = bottomNavigationBackgroundLight,
+    onSurface = backgroundSecondaryLight,
 )
 
 @Composable
 fun TempusTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit,
 ) {
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
