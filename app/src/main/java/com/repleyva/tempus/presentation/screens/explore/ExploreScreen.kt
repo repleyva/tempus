7a8848cb35661d6c5f268ba7a933df400f28d70b
repleyva <rusvelpassos@gmail.com>
2025-01-02 19:14:08 -0500

    feature: add ExploreScreen

---
+++ app/src/main/java/com/repleyva/tempus/presentation/screens/explore/ExploreScreen.kt
@@ -0,0 +1,10 @@
+package com.repleyva.tempus.presentation.screens.explore
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ExploreScreen(modifier: Modifier = Modifier) {
+    Text("ExploreScreen")
+}
