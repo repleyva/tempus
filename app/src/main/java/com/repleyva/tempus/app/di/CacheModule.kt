a9ee996ea7dc03149ad6b56f889eb8a8ca1e6b7f
repleyva <rusvelpassos@gmail.com>
2025-01-03 15:43:44 -0500

    feature: clean context into article manager

--- app/src/main/java/com/repleyva/tempus/app/di/CacheModule.kt
+++ app/src/main/java/com/repleyva/tempus/app/di/CacheModule.kt
@@ -1,12 +1,10 @@
 package com.repleyva.tempus.app.di
 
-import android.content.Context
 import com.repleyva.tempus.data.manager.ArticleCacheManagerImpl
 import com.repleyva.tempus.domain.manager.ArticleCacheManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -20,7 +18,6 @@ object CacheModule {
 
     @Singleton
     @Provides
-    fun provideArticleCacheManager(@ApplicationContext context: Context): ArticleCacheManager {
-        return ArticleCacheManagerImpl(context)
-    }
+    fun provideArticleCacheManager(): ArticleCacheManager = ArticleCacheManagerImpl()
+
 }
