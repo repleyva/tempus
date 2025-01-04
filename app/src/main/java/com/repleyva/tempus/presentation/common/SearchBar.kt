172c52db374c0f8fcb46256dd11ad85229ea4587
repleyva <rusvelpassos@gmail.com>
2025-01-04 18:00:15 -0500

    feature: add SearchBar

---
+++ app/src/main/java/com/repleyva/tempus/presentation/common/SearchBar.kt
@@ -0,0 +1,130 @@
+package com.repleyva.tempus.presentation.common
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.repleyva.tempus.R
+import com.repleyva.tempus.presentation.theme.Dimensions.iconLarge
+import com.repleyva.tempus.presentation.theme.TempusTheme
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    text: String,
+    readOnly: Boolean,
+    onClick: (() -> Unit)? = null,
+    onValueChange: (String) -> Unit,
+    onSearch: () -> Unit,
+    onSearchClosed: () -> Unit,
+) {
+
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val isClicked = interactionSource.collectIsPressedAsState().value
+
+    LaunchedEffect(key1 = isClicked) {
+        if (isClicked) {
+            onClick?.invoke()
+        }
+    }
+
+    TextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .searchBarBorder(),
+        value = text,
+        onValueChange = onValueChange,
+        readOnly = readOnly,
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_search),
+                contentDescription = null,
+                modifier = Modifier.size(iconLarge),
+                tint = MaterialTheme.colorScheme.tertiary
+            )
+        },
+        placeholder = {
+            Text(
+                text = "Search",
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.secondary
+            )
+        },
+        trailingIcon = {
+            IconButton(onClick = {
+                if (text.isNotEmpty()) {
+                    onValueChange("")
+                } else onSearchClosed()
+            }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_close),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            }
+        },
+        shape = MaterialTheme.shapes.medium,
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.onSurface,
+            focusedContainerColor = MaterialTheme.colorScheme.onSurface,
+            disabledContainerColor = MaterialTheme.colorScheme.onSurface,
+            focusedTextColor = MaterialTheme.colorScheme.tertiary,
+            unfocusedTextColor = MaterialTheme.colorScheme.tertiary,
+            cursorColor = MaterialTheme.colorScheme.tertiary,
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent
+        ),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = { onSearch() }),
+        textStyle = MaterialTheme.typography.bodySmall,
+        interactionSource = interactionSource
+    )
+}
+
+fun Modifier.searchBarBorder() = composed {
+    if (!isSystemInDarkTheme()) {
+        border(
+            width = 1.dp,
+            color = MaterialTheme.colorScheme.secondary,
+            shape = MaterialTheme.shapes.medium
+        )
+    } else {
+        this
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun SearchBarPreview() {
+    TempusTheme(
+        dynamicColor = false
+    ) {
+        SearchBar(text = "", readOnly = false, onValueChange = {}, onSearch = {}) {}
+    }
+}
