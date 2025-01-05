6efab133060d42f32e4932be558b88bcf510dfae
repleyva <rusvelpassos@gmail.com>
2025-01-05 17:11:02 -0500

    feature: move component ArticleDetails

--- app/src/main/java/com/repleyva/tempus/presentation/screens/explore/components/ExploreCard.kt
+++ app/src/main/java/com/repleyva/tempus/presentation/screens/explore/components/ExploreCard.kt
@@ -6,24 +6,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.repleyva.tempus.R
 import com.repleyva.tempus.domain.constants.Constants.dummyArticle
 import com.repleyva.tempus.domain.model.Article
-import com.repleyva.tempus.presentation.common.CardSourceTextSmall
+import com.repleyva.tempus.presentation.common.ArticleDetails
 import com.repleyva.tempus.presentation.common.CardTitleTextSmall
 import com.repleyva.tempus.presentation.common.ExploreArticleImage
-import com.repleyva.tempus.presentation.extensions.formatDate
-import com.repleyva.tempus.presentation.theme.Dimensions.iconSmall
 import com.repleyva.tempus.presentation.theme.Dimensions.paddingExtraSmall
 import com.repleyva.tempus.presentation.theme.Dimensions.paddingMedium
 import com.repleyva.tempus.presentation.theme.Dimensions.paddingSmall
@@ -77,33 +69,6 @@ private fun ArticleDescription(
         )
 
         ArticleDetails(article)
-    }
-}
-
-@Composable
-private fun ArticleDetails(article: Article) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(paddingExtraSmall)
-    ) {
-
-        CardSourceTextSmall(
-            modifier = Modifier.padding(end = paddingExtraSmall),
-            text = article.source.name,
-            style = MaterialTheme.typography.labelSmall.copy(fontWeight = FontWeight.SemiBold)
-        )
-
-        Icon(
-            painter = painterResource(id = R.drawable.ic_time),
-            contentDescription = null,
-            modifier = Modifier.size(iconSmall),
-            tint = MaterialTheme.colorScheme.secondary
-        )
-
-        CardSourceTextSmall(
-            text = article.publishedAt.orEmpty().formatDate(),
-            style = MaterialTheme.typography.labelSmall
-        )
     }
 }
 
