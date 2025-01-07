cf1b918476b7788ea6d80016dc070987451ab57c
repleyva <rusvelpassos@gmail.com>
2025-01-07 13:23:17 -0500

    feature: add settings screen

--- app/src/main/java/com/repleyva/tempus/presentation/extensions/ContextExtensions.kt
+++ app/src/main/java/com/repleyva/tempus/presentation/extensions/ContextExtensions.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import com.repleyva.tempus.R
+import com.repleyva.tempus.domain.model.AppVersion
+import com.repleyva.tempus.domain.model.DarkMode
+import com.repleyva.tempus.domain.model.Timezone
 import java.util.Calendar
 import java.util.TimeZone
 
@@ -42,3 +45,36 @@ fun Context.shareUrl(url: String) {
         }
     }
 }
+
+fun Context.darkMode(
+    isDarkModeEnabled: Boolean,
+) = DarkMode(
+    title = getString(R.string.dark_mode),
+    description =
+    if (isDarkModeEnabled) getString(R.string.dark_mode_on)
+    else getString(R.string.dark_mode_off),
+    isDarkModeEnabled = isDarkModeEnabled,
+    icon =
+    if (isDarkModeEnabled) R.drawable.ic_mode_dark
+    else R.drawable.ic_mode_light
+)
+
+fun Context.timeZone(
+    selectedTimezone: String,
+) = Timezone(
+    title = getString(R.string.timezone),
+    description = selectedTimezone,
+    icon = R.drawable.ic_timezone
+)
+
+fun Context.appVersion(
+    appVersion: String,
+) = AppVersion(
+    title = getString(R.string.app_version),
+    description = appVersion,
+    icon = R.drawable.ic_about,
+)
+
+fun Context.getAppVersionName(): String = runCatching {
+    packageManager.getPackageInfo(packageName, 0).versionName
+}.getOrElse { "" }
