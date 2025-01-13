97e53ffbc70382ced8a7012daab513eed4b29c1d
repleyva <rusvelpassos@gmail.com>
2025-01-13 16:38:25 -0500

    feature: add FlowHelper.kt

---
+++ app/src/main/java/com/repleyva/tempus/domain/common/FlowHelper.kt
@@ -0,0 +1,28 @@
+package com.repleyva.tempus.domain.common
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+fun <T> Flow<T>.asCommonFlow(): CommonFlow<T> = CommonFlow(this)
+
+class CommonFlow<T>(private val origin: Flow<T>) : Flow<T> by origin {
+
+    fun collectCommon(
+        coroutineScope: CoroutineScope?,
+        callback: suspend (T) -> Unit,
+    ) = onEach(callback::invoke)
+        .launchIn(coroutineScope ?: CoroutineScope(Main))
+}
+
+
+
+
+
+
+
+
+
+
