ca9f49d233dd66460b58f8cff9855ff7658f09ba
repleyva <rusvelpassos@gmail.com>
2025-01-13 16:39:26 -0500

    feature: refactor HomeViewModel.kt

--- app/src/main/java/com/repleyva/tempus/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/repleyva/tempus/presentation/screens/home/HomeViewModel.kt
@@ -5,20 +5,21 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.repleyva.tempus.data.remote.api.WeatherApi
+import com.repleyva.tempus.domain.common.DataState
 import com.repleyva.tempus.domain.constants.CacheConstants.BREAKING_NEWS
 import com.repleyva.tempus.domain.constants.CacheConstants.EVERYTHING_NEWS
 import com.repleyva.tempus.domain.constants.Constants.COUNTRY_PREFIX_DEFAULT
 import com.repleyva.tempus.domain.constants.Constants.FIVE_MINUTES_MILLIS
 import com.repleyva.tempus.domain.constants.Constants.SOURCES
-import com.repleyva.tempus.domain.constants.Constants.WEATHER_KEY
-import com.repleyva.tempus.domain.constants.Constants.WEATHER_URL
+import com.repleyva.tempus.domain.errors.orGenericError
 import com.repleyva.tempus.domain.extensions.filterArticles
 import com.repleyva.tempus.domain.extensions.timezoneToCity
 import com.repleyva.tempus.domain.manager.ArticleCacheManager
 import com.repleyva.tempus.domain.model.Article
 import com.repleyva.tempus.domain.use_cases.news.NewsUseCases
+import com.repleyva.tempus.domain.use_cases.weather.GetWeather
 import com.repleyva.tempus.presentation.base.SimpleMVIBaseViewModel
+import com.repleyva.tempus.presentation.extensions.timezoneToCity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.ProducerScope
@@ -27,15 +28,13 @@ import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val newsUseCases: NewsUseCases,
     private val articleCacheManager: ArticleCacheManager,
+    private val getWeather: GetWeather,
     private val dataStore: DataStore<Preferences>,
 ) : SimpleMVIBaseViewModel<HomeState, HomeEvent>() {
 
@@ -46,13 +45,11 @@ class HomeViewModel @Inject constructor(
 
     override fun eventHandler(event: HomeEvent) {
         when (event) {
-            is HomeEvent.UpdateCity -> updateCity()
-            is HomeEvent.FetchWeatherData -> fetchWeatherData()
+            is HomeEvent.FetchWeatherData -> fetchWeatherData(event.city)
         }
     }
 
     init {
-        fetchWeatherData()
         startAutoRefresh()
     }
 
@@ -94,36 +91,17 @@ class HomeViewModel @Inject constructor(
             }
     }
 
-    private fun updateCity() {
+    private fun fetchWeatherData(city: String?) {
         viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val city = dataStore.timezoneToCity()
-                println("Updating weather data for new city: $city")
-                fetchWeatherData()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    private fun fetchWeatherData() {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val city = dataStore.timezoneToCity()
-                println("Fetching weather data for city: $city")
-                val apiKey = WEATHER_KEY
-                val weatherApi: WeatherApi = Retrofit.Builder()
-                    .baseUrl(WEATHER_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
-                    .create(WeatherApi::class.java)
-
-                val fetchedWeatherData = weatherApi.getWeather(city, apiKey)
-                updateUi { copy(weatherData = fetchedWeatherData) }
-            } catch (e: HttpException) {
-                e.printStackTrace()
-            } catch (e: Exception) {
-                e.printStackTrace()
+            val cityCached = if (city.isNullOrEmpty()) dataStore.timezoneToCity() else city.timezoneToCity()
+            getWeather(cityCached).collectCommon(viewModelScope) { dataState ->
+                updateUi {
+                    when (dataState) {
+                        is DataState.Data -> copy(weatherData = dataState.data, isWeatherLoading = false)
+                        is DataState.Loading -> copy(isWeatherLoading = dataState.isLoading)
+                        is DataState.Error -> copy(weatherError = dataState.error.orGenericError(), isWeatherLoading = false)
+                    }
+                }
             }
         }
     }
