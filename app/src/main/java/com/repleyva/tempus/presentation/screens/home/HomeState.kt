635d5bd2d04d81f3d6bcf8485012df9c68878676
repleyva <rusvelpassos@gmail.com>
2025-01-13 16:39:14 -0500

    feature: refactor HomeState.kt

--- app/src/main/java/com/repleyva/tempus/presentation/screens/home/HomeState.kt
+++ app/src/main/java/com/repleyva/tempus/presentation/screens/home/HomeState.kt
@@ -1,6 +1,7 @@
 package com.repleyva.tempus.presentation.screens.home
 
 import androidx.paging.PagingData
+import com.repleyva.tempus.domain.errors.ErrorDomain
 import com.repleyva.tempus.domain.model.Article
 import com.repleyva.tempus.domain.model.WeatherData
 import com.repleyva.tempus.presentation.base.State
@@ -10,5 +11,7 @@ import kotlinx.coroutines.flow.emptyFlow
 data class HomeState(
     val breakingNews: Flow<PagingData<Article>> = emptyFlow(),
     val everythingNews: Flow<PagingData<Article>> = emptyFlow(),
-    val weatherData: WeatherData? = null
+    val weatherData: WeatherData? = null,
+    val isWeatherLoading: Boolean = false,
+    val weatherError: ErrorDomain? = null,
 ) : State
